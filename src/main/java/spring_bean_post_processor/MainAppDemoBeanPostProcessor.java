b2f9ba847f7f127f7940c8293650c45a6bdbc3d1
Fisher Coder <fishercoder@gmail.com>
2024-01-20 09:16:52 -0800

    update variable names

--- src/main/java/spring_bean_post_processor/MainAppDemoBeanPostProcessor.java
+++ src/main/java/spring_bean_post_processor/MainAppDemoBeanPostProcessor.java
@@ -1,6 +1,9 @@
 package spring_bean_post_processor;
 
-/**This MainApp is working fine.*/
+/**
+ * This MainApp is working fine.
+ */
+
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -10,13 +13,13 @@ public class MainAppDemoBeanPostProcessor {
         AbstractApplicationContext context = new ClassPathXmlApplicationContext(
                 "beansForDemoBeanPostProcessor.xml");
 
-        common.HelloWorld obj = (common.HelloWorld) context.getBean("helloWorld");
-        obj.getMessage();
-        
-        common.HelloChina obj2 = (common.HelloChina) context.getBean("helloChina");
-        obj2.setMessage("China is saying hello to the rest of the world!");
-//        obj2.getMessage();
-        
+        common.HelloWorld helloWorld = (common.HelloWorld) context.getBean("helloWorld");
+        helloWorld.getMessage();
+
+        common.HelloChina helloChina = (common.HelloChina) context.getBean("helloChina");
+        helloChina.setMessage("China is saying hello to the rest of the world!");
+        helloChina.getMessage();
+
         context.registerShutdownHook();
 
         System.out.println("The program ended! Cool!");
